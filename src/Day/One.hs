f2cce1c9c46ced54c4cdf744fff5779372966ba9
Jane Keibler <wanderingelf14@gmail.com>
2017-12-02 13:46:00 -0500

    Advent of Code - Refactoring
    Added -Wall warnings
    Made the sum return a Maybe value instead of zero if they are not equal

--- src/Day/One.hs
+++ src/Day/One.hs
@@ -1,15 +1,16 @@
 module Day.One (solveCaptcha
                , solveOppositeCaptcha) where
 
+import Data.Maybe (catMaybes)
 import Data.List (group, zipWith)
 
 solveCaptcha :: (Num a, Eq a) => [a] -> a
-solveCaptcha captcha = sum $ fmap sumNext $ filter ((>1) . length ) $ group loopedCaptcha
+solveCaptcha captcha = sum $ fmap sumNext $ filter ((>1) . length) $ group loopedCaptcha
   where loopedCaptcha = captcha ++ [head captcha]
         sumNext nums = sum $ tail nums
 
 solveOppositeCaptcha :: (Num a, Eq a) => [a] -> a
-solveOppositeCaptcha captcha = sum $ fmap sumIfEqual pairedDigits
+solveOppositeCaptcha captcha = sum $ catMaybes $ fmap sumIfEqual pairedDigits
   where halfLength = length captcha `quot` 2
         pairedDigits = zipWith (\a b -> (a, captcha !! mod (b + halfLength) (length captcha))) captcha [0..]
-        sumIfEqual (a, b) = if a == b then a else 0
+        sumIfEqual (a, b) = if a == b then Just a else Nothing
--- test/Day/OneSpec.hs
+++ test/Day/OneSpec.hs
@@ -8,21 +8,21 @@ spec :: Spec
 spec = do
   describe "solve captcha" $ do
     it "handles 1122" $
-      solveCaptcha [1, 1, 2, 2] `shouldBe` 3
+      solveCaptcha [1, 1, 2, 2] `shouldBe` (3 :: Integer)
     it "handles 1111" $
-      solveCaptcha [1, 1, 1, 1] `shouldBe` 4
+      solveCaptcha [1, 1, 1, 1] `shouldBe` (4 :: Integer)
     it "handles 1234" $
-      solveCaptcha [1, 2, 3, 4] `shouldBe` 0
+      solveCaptcha [1, 2, 3, 4] `shouldBe` (0 :: Integer)
     it "handles 91212129" $
-      solveCaptcha [9,1,2,1,2,1,2,9] `shouldBe` 9
+      solveCaptcha [9,1,2,1,2,1,2,9] `shouldBe` (9 :: Integer)
   describe "solve opposite captcha" $ do
     it "handles 1212" $
-      solveOppositeCaptcha [1,2,1,2] `shouldBe` 6
+      solveOppositeCaptcha [1,2,1,2] `shouldBe` (6 :: Integer)
     it "handles 1221" $
-      solveOppositeCaptcha [1,2,2,1] `shouldBe` 0
+      solveOppositeCaptcha [1,2,2,1] `shouldBe` (0 :: Integer)
     it "handles 123425" $
-      solveOppositeCaptcha [1,2,3,4,2,5] `shouldBe` 4
+      solveOppositeCaptcha [1,2,3,4,2,5] `shouldBe` (4 :: Integer)
     it "handles 123123" $
-      solveOppositeCaptcha [1,2,3,1,2,3] `shouldBe` 12
+      solveOppositeCaptcha [1,2,3,1,2,3] `shouldBe` (12 :: Integer)
     it "handles 12131415" $
-      solveOppositeCaptcha [1,2,1,3,1,4,1,5] `shouldBe` 4
+      solveOppositeCaptcha [1,2,1,3,1,4,1,5] `shouldBe` (4 :: Integer)
